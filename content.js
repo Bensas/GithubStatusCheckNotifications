8c515b64d14bb3ce6f460450972c0346b9063664
Bensas <juanbensa@gmail.com>
2023-11-10 14:40:52 -0300

    Revamped content.js to support cases where the PR might momentarily show that checs have passed even though they still haven't finished running.

--- content.js
+++ content.js
@@ -4,72 +4,80 @@ const STATUS_PASSED = 'passed';
 const STATUS_EMPTY = 'empty';
 
 window.onload = function(){
-  checkForNotificationSupport();
+    checkForNotificationSupport();
 
-  var currentStatus;
-  var statusMessageElements = Array.from(document.getElementsByClassName('status-heading'));
+    let latestStatus = {"status": STATUS_EMPTY, "time": new Date()}
+    
+    var interval = setInterval(function() {
+        let currentStatus = determineCurrentStatus()
+        if (latestStatus.status !== currentStatus) {
+            if (latestStatus.status === STATUS_EMPTY && (currentStatus === STATUS_FAILED || currentStatus === STATUS_PASSED)) {
+                return
+            }
+            latestStatus.status = currentStatus;
+            latestStatus.time = new Date()
+        }
+        
+        if (currentStatus === STATUS_FAILED || currentStatus === STATUS_PASSED) {
+            let currentTime = new Date()
+            if (currentTime - latestStatus.time > 3000) {
+                sendNotification(latestStatus.status, window.location.href.split('/pull/')[1])
+                clearInterval(interval)
+            }
+        }
 
-  currentStatus = getStatus(statusMessageElements);
-  setInterval(function() {
-    prevStatus = currentStatus;
-    statusMessageElements = Array.from(document.getElementsByClassName('status-heading'));
-    currentStatus = getStatus(statusMessageElements);
-    if (currentStatus !== prevStatus && currentStatus !== STATUS_RUNNING && prevStatus !== STATUS_EMPTY){
-      var prNumber = window.location.href.split('/pull/')[1];
-      if (!isNaN(prNumber)){
-        sendNotification(currentStatus, prNumber);
-      }
-    }
-  },  5000); //Called every 5 seconds
-};
-
-sendNotification = function(status, prNumber){
-  var notification;
-  if (status === STATUS_PASSED){
-    notification = new Notification('All checks have passed for PR ' + prNumber, {
-      icon: chrome.runtime.getURL("parrot.gif"),
-      body: 'It\'s review time!',
-    });
-  }
-  else if (status === STATUS_FAILED){
-    notification = new Notification('Some checks have failed for PR ' + prNumber, {
-      icon: chrome.runtime.getURL("sadparrot.gif"),
-      body: 'It\'s whitespace-fixing time!',
-     });
-  }
-  else {
-    return;
-  }
-  notification.onclick = function() {
-   window.open(window.location.href);
+    },  1000); // Called every second
   };
+
+function sendNotification(status, prNumber) {
+    var notification;
+    if (status === STATUS_PASSED){
+        notification = new Notification('All checks have passed for PR ' + prNumber, {
+        icon: chrome.runtime.getURL("parrot.gif"),
+        body: 'It\'s review time!',
+        });
+    }
+    else if (status === STATUS_FAILED){
+        notification = new Notification('Some checks have failed for PR ' + prNumber, {
+        icon: chrome.runtime.getURL("sadparrot.gif"),
+        body: 'It\'s whitespace-fixing time!',
+        });
+    }
+    else {
+        return;
+    }
+    notification.onclick = function() {
+        window.open(window.location.href);
+    };
 }
 
-getStatus = function(statusElements){
-  for (var i = 0; i < statusElements.length; i++){
-    if (elemContainsText(statusElements[i], 'Some checks were not successful') ||
-        elemContainsText(statusElements[i],'All checks have failed')){
-      return STATUS_FAILED;
-    }
-    else if (elemContainsText(statusElements[i],'Some checks haven’t completed yet')){
-      return STATUS_RUNNING;
-    }
-    else if (elemContainsText(statusElements[i], 'All checks have passed')){
-      return STATUS_PASSED;
-    }
-  }
-  return STATUS_EMPTY;
+function checkForNotificationSupport() {
+    if (!Notification) {
+        alert('Desktop notifications not available in your browser. Try Chromium.');
+        return;
+        }
+    if (Notification.permission !== 'granted')
+        Notification.requestPermission();
+}
+  
+
+function determineCurrentStatus() {
+    let statusMessageElements = Array.from(document.getElementsByClassName('status-heading'));
+    for (var i = 0; i < statusMessageElements.length; i++){
+        if (elemContainsText(statusMessageElements[i], 'Some checks were not successful') ||
+            elemContainsText(statusMessageElements[i],'All checks have failed')){
+          return STATUS_FAILED;
+        }
+        else if (elemContainsText(statusMessageElements[i],'Some checks haven’t completed yet')){
+          return STATUS_RUNNING;
+        }
+        else if (elemContainsText(statusMessageElements[i], 'All checks have passed')){
+          return STATUS_PASSED;
+        }
+      }
+      return STATUS_EMPTY;
 }
 
-elemContainsText = function(elem, text){
-  return elem.innerHTML.indexOf(text) !== -1;
-}
-
-checkForNotificationSupport = function(){
-  if (!Notification) {
-    alert('Desktop notifications not available in your browser. Try Chromium.');
-    return;
-   }
-  if (Notification.permission !== 'granted')
-    Notification.requestPermission();
+function elemContainsText(elem, text) {
+    return elem.innerHTML.indexOf(text) !== -1;
 }
